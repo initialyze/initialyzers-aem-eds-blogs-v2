b595aebb13ccbe32b47fb26e63df997f54039306
Venish Patel <wanicepatel.1210@gmail.com>
2025-04-09 14:03:42 -0700

    Refactored filtering logic.

--- blocks/filterable-list/filterable-list.js
+++ blocks/filterable-list/filterable-list.js
@@ -1,18 +1,42 @@
 import { createOptimizedPicture, getMetadata } from '../../scripts/aem.js';
-import { dashedText } from '../../scripts/utils.js';
+import { dashedText, formattedTagsArray } from '../../scripts/utils.js';
 
 const selectedfilters = new Set();
+
+const EVENTS = {
+  FILTER_UPDATED: 'events.filterable-list.filters.updated'
+}
 
 function handleSelect(e, filterVal, checked) {
   checked
     ? selectedfilters.add(filterVal)
     : selectedfilters.delete(filterVal);
-  
-  window.dispatchEvent(new CustomEvent("filters.updated", {
+
+  window.dispatchEvent(new CustomEvent(EVENTS.FILTER_UPDATED, {
     detail: {
       filters: selectedfilters
     }
   }));
+}
+
+function handleFilterUpdate(e, block) {
+  const updatedfilters = e.detail?.filters;
+  const items = block.querySelectorAll(".filter-card");
+
+  items.forEach((item) => {
+    const filterIds = JSON.parse(item.getAttribute("data-filter-ids"));
+    if (updatedfilters.size === 0) {
+      // If no filters are selected, show all items
+      item.classList.remove("hide");
+      return;
+    }
+
+    const hasMatchingFilter = filterIds.some((filterId) => updatedfilters.has(filterId));
+
+    (hasMatchingFilter)
+      ? item.classList.remove("hide")
+      : item.classList.add("hide");
+  });
 }
 
 function decorateCheckboxFilter(filterLabel, filterArray, filteableListWrapper) {
@@ -34,16 +58,15 @@ function decorateCheckboxFilter(filterLabel, filterArray, filteableListWrapper) 
   filteableListWrapper.appendChild(checkboxFilter);
 }
 
-export default async function decorate(elm) {
-  const a = elm.querySelector('a');
+export default async function decorate(block) {
+  const a = block.querySelector('a');
   const filterDataURL = a.href;
   const dataReq = await fetch(filterDataURL);
   const { data } = await dataReq.json();
   const currentLocale = getMetadata('locale');
-  console.log("locale", currentLocale);
   const filteredData = data.filter((filterData) => filterData.path.includes(currentLocale));
 
-  elm.innerHTML = '';
+  block.innerHTML = '';
   [...filteredData].forEach(({
     path, title, description, image, author, tags, locale
   }) => {
@@ -51,71 +74,34 @@ export default async function decorate(elm) {
     const filterCardTemplate = `<div class='filter-card-picture'><picture/></div>
     <div class='filter-card-content'><h3 class='filter-card-title'>${title}</h3>
     <p class='filter-card-desc'>${description}</p></div>`;
-    
-    const cleanedTagsString = String(tags)
-        .replace(/^\["|"\]$/g, '') // Remove leading [" and trailing "]
-        .replace(/"\s*,\s*"/g, ','); // Remove quotes around comma and any surrounding whitespace
-
-    const blogTags = cleanedTagsString.split(',').map(tag => tag.trim());
-
-    const blogTagsArr = [];
-    blogTags.forEach((tag) => {
-      blogTagsArr.push(`tags:${dashedText(tag)}`);
-    })
-      
-    const filterIds = [`authors:${dashedText(author)}`, ...blogTagsArr];
+    const tagsArr = formattedTagsArray(tags);
+    const tagIdsArr = tagsArr.map((tag) => `tags:${dashedText(tag)}`);
+    const filterIds = [`authors:${dashedText(author)}`, ...tagIdsArr];
 
     filterCard.className = 'filter-card';
     filterCard.href = path;
     filterCard.setAttribute('data-filter-ids', JSON.stringify(filterIds));
     filterCard.innerHTML = filterCardTemplate;
     filterCard.querySelector('picture').replaceWith(createOptimizedPicture(image, title, false, [{ width: '750', height: '750' }]));
-    elm.appendChild(filterCard);
+    block.appendChild(filterCard);
   });
 
-  const filteableListWrapper = elm.parentElement;
+  const filteableListWrapper = block.parentElement;
   if (filteableListWrapper.classList.contains('filterable-list-wrapper')) {
     const authors = filteredData.map((blog) => blog.author);
-    decorateCheckboxFilter('authors', authors, filteableListWrapper);
-
     const tags = filteredData.reduce((allTags, blog) => {
       if (blog.tags) {
-        const cleanedTagsString = String(blog.tags)
-          .replace(/^\["|"\]$/g, '') // Remove leading [" and trailing "]
-          .replace(/"\s*,\s*"/g, ','); // Remove quotes around comma and any surrounding whitespace
-  
-        const blogTags = cleanedTagsString.split(',').map(tag => tag.trim());
-        return allTags.concat(blogTags);
+        const tagsArr = formattedTagsArray(blog.tags);
+        return allTags.concat(tagsArr);
       }
       return allTags;
     }, []);
-    const uniqueTags = [...new Set(tags)];  
-    decorateCheckboxFilter("tags", uniqueTags, filteableListWrapper);  
-    
+    const uniqueTags = [...new Set(tags)];
+
+    decorateCheckboxFilter('authors', authors, filteableListWrapper);
+    decorateCheckboxFilter("tags", uniqueTags, filteableListWrapper);
+
   }
 
-  window.addEventListener("filters.updated", (e) => {
-    const filters = e.detail?.filters;
-    console.log("filters", filters);
-    const items = elm.querySelectorAll(".filter-card");
-
-    items.forEach((item) => {
-      const filterIds = JSON.parse(item.getAttribute("data-filter-ids"));
-      console.log("filters", filters)
-      console.log("filterIds", filterIds)
-      if (filters.size === 0) {
-        item.classList.remove("hide");
-      } else {
-        let isHide = true;
-        let i = 0;
-        while (isHide && i < filterIds.length) {
-          item.classList.add("hide");
-          if (filters.has(filterIds[i++])) {
-            item.classList.remove("hide");
-            isHide = false;
-          } 
-        }
-      }
-    });
-  });
+  window.addEventListener(EVENTS.FILTER_UPDATED, (e) => handleFilterUpdate(e, block));
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -2,6 +2,15 @@ function dashedText(text) {
   return text.toLowerCase().split(" ").join("-");
 }
 
+
+function formattedTagsArray(tags) {
+  return String(tags)
+    .replace(/^\["|"\]$/g, '') // Remove leading [" and trailing "]
+    .replace(/"\s*,\s*"/g, ',') // Remove quotes around comma and any surrounding whitespace
+    .split(',').map(tag => tag.trim())
+}
+
 export {
-  dashedText
+  dashedText,
+  formattedTagsArray,
 };
